6564224e173626d828703ab4536b9127a7430842
Yasir1st <yasirnakano2nd@gmail.com>
2022-10-08 22:10:43 +0800

    first commit

---
+++ conn.php
@@ -0,0 +1,8 @@
+<?php 
+    $conn = mysqli_connect("localhost","root","","crud");
+
+    // if(!$conn){
+    //     die("Connection failed: " . mysqli_connect_error());
+    // }echo "Connected succesfully";
+    // mysqli_close($conn);
+?>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/logo.css">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <img src="img/download.png">
+            <div>
+                <h1>Pendaftaran Mahasiswa Baru</h1>
+                <h3>Universitas Sulawesi Barat</h3>
+            </div>   
+        </header>
+
+        <div class="menu">
+            <nav class="nav">
+                <h3>Menu</h3>
+                <div></div>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="create.php">Daftar Baru</a></li>
+                    <li><a href="read.php">Pendaftar</a></li>
+                </ul>
+            </nav>
+
+            <div class="main" style="background-image: url('img/rektorat.jpg'); background-size: cover;
+    background-position: center;">
+                <div class="welcome">
+                    <h1>Welcome To Site</h1>
+                    <div class="line"></div>
+                </div>
+            </div>
+        </div>
+        
+        <footer class="footer">
+            <p>©2022 Form Data BY Yasir </p>
+        </footer>
+    </div>
+</body>
+</html>
